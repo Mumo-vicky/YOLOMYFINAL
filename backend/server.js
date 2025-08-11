d663be6f500c36cdf393a513cf1af20e5a8a88d6
Mumo-vicky <mumovicky00@gmail.com>
2025-08-12 02:40:52 +0300

    Files update for port connectivity

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,12 @@ const upload = multer();
 
 const productRoute = require('./routes/api/productRoute');
 
-// // Connecting to the Database
-// let mongodb_url = 'mongodb://localhost/';
-// let dbName = 'yolomy';
+// Connecting to the Database
+let mongodb_url = 'mongodb://localhost/';
+let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo/yolomy'; 
-// 'mongo' = Kubernetes service name
+const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
